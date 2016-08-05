d2dacd6f60508e592a12a19bf7c6e30992856c66
Facundo Navarro <facunava92@gmail.com>
2016-08-05 09:40:48 -0300

    Desde UCC

--- Debug/HAL_Driver/Src/stm32f4xx_hal_spi.d
+++ Debug/HAL_Driver/Src/stm32f4xx_hal_spi.d
@@ -1,190 +1,190 @@
 HAL_Driver/Src/stm32f4xx_hal_spi.o: ../HAL_Driver/Src/stm32f4xx_hal_spi.c \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_conf.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_def.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/device/stm32f4xx.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/device/stm32f407xx.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/core_cm4.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/core_cmInstr.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/cmsis_gcc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/core_cmFunc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/core_cmSimd.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/device/system_stm32f4xx.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dma.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_adc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_can.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_crc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_cryp.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dac.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dac_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_eth.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_sram.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_fsmc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_nor.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_nand.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pccard.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_hash.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rng.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_sd.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_spi.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_tim.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_usart.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_irda.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_usb.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
- C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_hcd.h
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Alumno/TPBlinky/CMSIS/device/stm32f4xx.h \
+ C:/Users/Alumno/TPBlinky/CMSIS/device/stm32f407xx.h \
+ C:/Users/Alumno/TPBlinky/CMSIS/core/core_cm4.h \
+ C:/Users/Alumno/TPBlinky/CMSIS/core/core_cmInstr.h \
+ C:/Users/Alumno/TPBlinky/CMSIS/core/cmsis_gcc.h \
+ C:/Users/Alumno/TPBlinky/CMSIS/core/core_cmFunc.h \
+ C:/Users/Alumno/TPBlinky/CMSIS/core/core_cmSimd.h \
+ C:/Users/Alumno/TPBlinky/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_can.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_crc.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_cryp.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dac.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dac_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_eth.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_sram.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_fsmc.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_nor.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_nand.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pccard.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_hash.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rng.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_sd.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_usart.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_irda.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
+ C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_hcd.h
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_conf.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_conf.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_def.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_def.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/device/stm32f4xx.h:
+C:/Users/Alumno/TPBlinky/CMSIS/device/stm32f4xx.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/device/stm32f407xx.h:
+C:/Users/Alumno/TPBlinky/CMSIS/device/stm32f407xx.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/core_cm4.h:
+C:/Users/Alumno/TPBlinky/CMSIS/core/core_cm4.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/core_cmInstr.h:
+C:/Users/Alumno/TPBlinky/CMSIS/core/core_cmInstr.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/cmsis_gcc.h:
+C:/Users/Alumno/TPBlinky/CMSIS/core/cmsis_gcc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/core_cmFunc.h:
+C:/Users/Alumno/TPBlinky/CMSIS/core/core_cmFunc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/core/core_cmSimd.h:
+C:/Users/Alumno/TPBlinky/CMSIS/core/core_cmSimd.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/CMSIS/device/system_stm32f4xx.h:
+C:/Users/Alumno/TPBlinky/CMSIS/device/system_stm32f4xx.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dma.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_adc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_can.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_can.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_crc.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_crc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_cryp.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_cryp.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dac.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dac.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dac_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dac_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_dcmi_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_eth.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_eth.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_sram.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_sram.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_fsmc.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_fsmc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_nor.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_nor.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_nand.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_nand.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pccard.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pccard.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_hash.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_hash.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rng.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rng.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_sd.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_sd.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_spi.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_tim.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_uart.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_usart.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_usart.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_irda.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_irda.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_ll_usb.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
 
-C:/Users/facun/OneDrive/Sistemas\ Embebidos/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+C:/Users/Alumno/TPBlinky/HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
